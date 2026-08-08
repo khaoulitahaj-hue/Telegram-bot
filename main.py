625316f970208d7ded717a04b99c907aba3d505e
khaoulitahaj-hue <khaoulitahaj@gmail.com>
2026-08-08 22:03:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from telegram.ext import Application, CommandHandler, CallbackQueryHandler, Cont
 logging.basicConfig(level=logging.INFO)
 
 # ==== إعدادات يجب تعديلها ====
-BOT_TOKEN = "8966926917:AAH_NfukVrf2efJwVREW9nOdQV311YtVeYY"          # التوكن من BotFather
+BOT_TOKEN = import os
+BOT_TOKEN = os.environ.get("BOT_TOKEN")          # التوكن من BotFather
 # لا حاجة لكتابة معرفات الأدمينات يدوياً — البوت يتحقق تلقائياً من تيليجرام
 # ==============================
 
